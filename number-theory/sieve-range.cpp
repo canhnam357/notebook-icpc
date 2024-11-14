e4349565b1d15c564e063dd0f2f85f27fca615aa
Canh Nam <canhnam357@gmail.com>
2024-11-14 23:38:01 +0700

    add

---
+++ number-theory/sieve-range.cpp
@@ -0,0 +1,12 @@
+int segmentedSieveNoPreGen(long long L, long long R) {
+    vector<bool> isPrime(R - L + 1, true);
+    long long lim = sqrt(R);
+    for (long long i = 2; i <= lim; ++i)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    int ans = R - L + 1;
+    for (int i : isPrime) ans -= !i;
+    return ans;
+}
