8a959389e098c36f62ac808fc5fa0a94d2f4b2cb
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-07-19 16:16:02 +0700

    Update aho-corasick.cpp

--- string/aho-corasick.cpp
+++ string/aho-corasick.cpp
@@ -6,11 +6,11 @@ using namespace std;
 struct aho_corasick{
     struct node{
         int suffix_link = -1, exit_link = -1, nxt[26];
-        vector<int> leaf;
+        bool has = false;
         node() {fill(nxt, nxt + 26, -1);}
     };
     vector<node> g = {node()};
-    void insert_string(const string &s, int sidx){
+    void insert_string(const string &s){
         int p = 0;
         for (char c: s){
             if (g[p].nxt[c - 'a'] == -1){
@@ -19,23 +19,12 @@ struct aho_corasick{
             }
             p = g[p].nxt[c - 'a'];
         }
-        g[p].leaf.push_back(sidx);
-    }
-    void cc()
-    {
-        for (int i = 0; i < g.size(); i++)
-        {
-            if (g[i].leaf.size())
-            {
-                sort(all(g[i].leaf));
-                g[i].leaf.erase(unique(all(g[i].leaf)), g[i].leaf.end());
-            }
-        }
+        g[p].has = true;
     }
     void build_automaton(){
         for (deque<int> q = {0}; q.size(); q.pop_front()){
             int v = q.front(), suffix_link = g[v].suffix_link;
-            if (v) g[v].exit_link = g[suffix_link].leaf.size() ? suffix_link : g[suffix_link].exit_link;
+            if (v) g[v].exit_link = g[suffix_link].has ? suffix_link : g[suffix_link].exit_link;
             for (int i=0; i< 26; i++){
                 int &nxt = g[v].nxt[i], nxt_sf = v ? g[suffix_link].nxt[i] : 0;
                 if (nxt == -1) nxt = nxt_sf;
@@ -46,14 +35,8 @@ struct aho_corasick{
             }
         }
     }
-    vector<int> get_sindex(int p){
-        vector<int> a;
-        for (int v = g[p].leaf.size() ? p : g[p].exit_link; v != -1; v = g[v].exit_link)
-            for (int j: g[v].leaf)
-                a.push_back(j);
-        sort(all(a));
-        a.erase(unique(all(a)), a.end());
-        return a;
+    int feed(int p, int c) {
+        return g[p].nxt[c];
     }
 };
 
