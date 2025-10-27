bba9714f07d76ba9224f6c5cf40305ae01cb7c44
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-10-27 11:33:06 +0700

    Update persistent-segment-tree.cpp

--- data-structures/persistent-segment-tree.cpp
+++ data-structures/persistent-segment-tree.cpp
@@ -1,72 +1,71 @@
-struct Node {
-    int l, r;
-    int val;
-    Node() : l(0), r(0), val(0) {};
-    Node(int l_, int r_, int val_) : l(l_), r(r_), val(val_) {};
-};
-struct PST
-{
-    vector<Node> tree;
-    vector<int> id_root;
-    int n, N, sz; // sz is power_of_2
-    PST() : n(0), N(0), sz(0) {}
-    PST(int n) : n(n) // max of n.o queries update and n.o values
-    {
-        N = 0;
-        sz = 1; 
-        while (sz / 2 < n + 5) sz *= 2;
-        sz // equal n.o values doubled
-        tree.resize(sz);
-        id_root.resize(n + 5); // equal n.o queries update
-        id_root[0] = 1;
-        build();
-    }
-    void build()
-    {
-        for (int i = 1; i < sz / 2; i++)
-        {
-            tree[i].l = i << 1;
-            tree[i].r = i << 1 | 1;
-        }
-    }
-    void update(int id, int l, int r, int x)
-    {
-        tree[id].val++;
-        if (l != r)
-        {
-            int mid = (l + r) >> 1;
-            int u = tree[id].l, v = tree[id].r;
-            if (x <= mid)
-            {
-                tree[id].l = tree.size();
-                tree.push_back(tree[u]);
-                update(tree[id].l, l, mid, x);
-            }
-            else
-            {
-                tree[id].r = tree.size();
-                tree.push_back(tree[v]);
-                update(tree[id].r, mid + 1, r, x);
-            }
-        }
-    }
-    int get_kth(int u, int v, int k, int l, int r) {
-        if (l == r) return l;
+#define N  1000000 // N must double of update or add
+#define LG 20 // log
+int L[N * LG], R[N * LG], val[N * LG], root[N];
+long long sum[N * LG];
+int M = 0; // number of roots
+int p = 2; // id mapped
+int n = 200001; // size range tree
+void update(int id, int l, int r, int x) {
+    val[id]++;
+    sum[id] += x;
+    if (l != r) {
         int mid = (l + r) >> 1;
-        int sz = tree[tree[v].l].val - tree[tree[u].l].val;
-        if (sz >= k) return get_kth(tree[u].l, tree[v].l, k, l, mid);
-        return get_kth(tree[u].r, tree[v].r, k - sz, mid + 1, r);
+        int u = L[id], v = R[id];
+        if (x <= mid) {
+            L[id] = p++;
+            tie(L[L[id]], R[L[id]], val[L[id]], sum[L[id]]) = {L[u], R[u], val[u], sum[u]};
+            update(L[id], l, mid, x);
+        }
+        else {
+            R[id] = p++;
+            tie(L[R[id]], R[R[id]], val[R[id]], sum[R[id]]) = {L[v], R[v], val[v], sum[v]};
+            update(R[id], mid + 1, r, x);
+        }
     }
-    int get_kth(int l, int r, int k)
-    {
-        return get_kth(id_root[l - 1], id_root[r], k, 1, sz / 2);
-    }
-    void create_node(int val)
-    {
-        id_root[++N] = tree.size(); 
-        // N is pos_update
-        // if use on tree, change ++N with current vertex, so N - 1 equal parent of this vertex
-        tree.push_back(tree[id_root[N - 1]]);
-        update(id_root[N], 1, sz / 2, val);
-    }
-};
+}
+void create_node(int x) {
+    root[++M] = p++;
+    tie(L[root[M]], R[root[M]], val[root[M]], sum[root[M]]) = {L[root[M - 1]], R[root[M - 1]], val[root[M - 1]], sum[root[M - 1]]};
+    update(root[M], 1, n, x);
+}
+long long get_occ(int v, int u, int x, int y, int l, int r) {
+    if (r < x || l > y) return 0;
+    if (l >= x && r <= y) return val[u] - val[v];
+    int mid = (l + r) >> 1;
+    return get_occ(L[v], L[u], x, y, l, mid) + get_occ(R[v], R[u], x, y, mid + 1, r);
+}
+long long get_occ(int l, int r, int x, int y) {
+    return get_occ(root[l - 1], root[r], x, y, 1, n);
+}
+long long get_sum(int v, int u, int x, int y, int l, int r) {
+    if (r < x || l > y) return 0;
+    if (l >= x && r <= y) return sum[u] - sum[v];
+    int mid = (l + r) >> 1;
+    return get_sum(L[v], L[u], x, y, l, mid) + get_sum(R[v], R[u], x, y, mid + 1, r);
+}
+long long get_sum(int l, int r, int x, int y) {
+    return get_sum(root[l - 1], root[r], x, y, 1, n);
+}
+long long get_kth(int v, int u, int x, int l, int r) {
+    if (l == r) return r;
+    int k = val[L[u]] - val[L[v]];
+    int mid = (l + r) >> 1;
+    if (k >= x) return get_kth(L[v], L[u], x, l, mid);
+    return get_kth(R[v], R[u], x - k, mid + 1, r);
+}
+long long get_kth(int l, int r, int x) {
+    return get_kth(root[l - 1], root[r], x, 1, n);
+}
+void build(int id, int l, int r) {
+    val[id] = sum[id] = 0;
+    if (l == r) return;
+    int mid = (l + r) >> 1;
+    L[id] = p++;
+    R[id] = p++;
+    build(L[id], l, mid);
+    build(R[id], mid + 1, r);
+}
+
+// root[0] = 1
+// build(1, 1, n);
+// node in order of call create_node 
