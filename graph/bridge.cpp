c338fb099dd359fb9d5cd5f664fdc2026765b660
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-11-05 23:38:38 +0700

    Update bridge.cpp

--- graph/bridge.cpp
+++ graph/bridge.cpp
@@ -1,25 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// https://www.acmicpc.net/problem/11400
-// https://oj.vnoi.info/problem/graph_
 // 0-indexed
 struct Bridge { // must be no multi-edge
 	int n, dfs_cnt;
 	vector<int> dfs_order;
 	vector<pair<int, int>> bridge;
 	vector<vector<int>> adj;
-
 	Bridge(int n) :
 		n(n), adj(n),
 		dfs_order(n),
 		dfs_cnt(0) {}
-
 	void AddEdge(int a, int b) {
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
-
 	int DFS(int cur, int prv = -1) {
 		int ret = dfs_order[cur] = ++dfs_cnt;
 		for (const auto& nxt : adj[cur]) {
@@ -33,7 +25,6 @@ struct Bridge { // must be no multi-edge
 		}
 		return ret;
 	}
-
 	void GetBridge() {
 		for (int i = 0; i < n; i++)
 			if (!dfs_order[i]) DFS(i);
@@ -41,22 +32,6 @@ struct Bridge { // must be no multi-edge
 		sort(bridge.begin(), bridge.end());
 	}
 };
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	int n, m;
-	cin >> n >> m;
-	Bridge b(n);
-	for (int i = 0; i < m; i++)
-	{
-		int u, v;
-		cin >> u >> v;
-		b.AddEdge(--u, --v);
-	}
-	b.GetBridge();
-	cout << b.bridge.size() << '\n';
-	for (auto [u, v] : b.bridge) cout << u + 1 << ' ' << v + 1 << '\n';
-	return 0;
-}
+// Bridge b(n);
+// b.AddEdge()
+// b.GetBridge();
