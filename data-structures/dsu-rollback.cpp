79c3395183786336f75974140dbb43b4fece3e07
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-11-05 22:46:36 +0700

    Update dsu-rollback.cpp

--- data-structures/dsu-rollback.cpp
+++ data-structures/dsu-rollback.cpp
@@ -2,8 +2,7 @@ struct DSUrb {
     vector<int> par, sz;
     stack<vector<int>> s;
     int cc;
-    void init(int n)
-    {
+    void init(int n) {
         cc = n;
         par.resize(n);
         sz.resize(n, 1);
@@ -13,20 +12,16 @@ struct DSUrb {
         if (x == par[x]) return x;
         return get(par[x]);
     }
-
     bool join(int u, int v) {
         u = get(u);
         v = get(v);
         if (u == v) return false;
-
         if (sz[u] < sz[v]) swap(u, v);
         par[v] = u;
-
         s.push({ u, v, sz[v] });
         sz[u] += sz[v];
         return true;
     }
-
     void rollback(int cnt) {
         while (cnt--) {
             auto a = s.top();
@@ -36,9 +31,7 @@ struct DSUrb {
             sz[a[1]] = a[2];
         }
     }
-
-    int same(int a, int b)
-    {
+    int same(int a, int b){
         return get(a) == get(b);
     }
 };
