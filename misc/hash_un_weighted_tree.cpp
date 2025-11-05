3d675f1f0275afe3ce0600e72175969a14a8b37b
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-11-05 23:04:12 +0700

    Update hash_un_weighted_tree.cpp

--- misc/hash_un_weighted_tree.cpp
+++ misc/hash_un_weighted_tree.cpp
@@ -1,30 +1,25 @@
 auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
 mt19937_64 mt(seed);
-long long get_random(long long l, long long r)
-{
+long long get_random(long long l, long long r) {
 	assert(l <= r);
 	return mt() % (r - l + 1) + l;
 }
-struct hash_unweighted_tree
-{
+struct hash_unweighted_tree{
 	// NOTE : undirected graph
 	const int mod1 = 2002577573;
 	const int mod2 = 2137321811;
 	int MAX_DEPTH;
 	vector<int> rnd;
-	void init(int MAX_DEPTH) // maybe number of vertex?
-	{
+	void init(int MAX_DEPTH) {// maybe number of vertex?
 		this->MAX_DEPTH = MAX_DEPTH;
 		rnd.resize(MAX_DEPTH);
 		for (int i = 0; i < MAX_DEPTH; i++) rnd[i] = get_random(1, min(mod1, mod2) - 1);
 	}
-	vector<int> Centroid(const vector<vector<int>> &adj) // get centroid, each tree have 1 or 2 centroid(s)
-	{
+	vector<int> Centroid(const vector<vector<int>> &adj){ // get centroid, each tree have 1 or 2 centroid(s)
 	    int n = adj.size();
 	    vector<int> centroid;
 	    vector<int> sz(n);
-	    function<void (int, int)> dfs = [&](int u, int p) 
-	    {
+	    function<void (int, int)> dfs = [&](int u, int p) {
             sz[u] = 1;
             bool is_centroid = true;
             for (auto v : adj[u]) if (v != p) {
@@ -38,14 +33,11 @@ struct hash_unweighted_tree
 	    dfs(0, -1);
 	    return centroid;
 	}
-	pair<int, int> get_hash(vector<vector<int>> adj, int root) // root = -1 if using centroid as root
-	{
+	pair<int, int> get_hash(vector<vector<int>> adj, int root) {// root = -1 if using centroid as root
 		int sz = adj.size();
-		if (root == -1)
-		{
+		if (root == -1){
 			auto c = Centroid(adj);
-			if (c.size() == 2)
-			{
+			if (c.size() == 2){
 				adj[c[0]].erase(find(all(adj[c[0]]), c[1]));
 				adj[c[1]].erase(find(all(adj[c[1]]), c[0]));
 				adj[c[0]].push_back(sz);
@@ -59,12 +51,10 @@ struct hash_unweighted_tree
 			else root = c[0];
 		}
 		vector<int> h1(sz, 1), h2(sz, 1), d(sz);
-		function<void(int, int)> dfs = [&](int u, int p)
-		{
+		function<void(int, int)> dfs = [&](int u, int p){
 			int has = 0;
 			vector<int> z1, z2;
-			for (int v : adj[u])
-			{
+			for (int v : adj[u]){
 				if (v == p) continue;
 				has = 1;
 				dfs(v, u);
@@ -75,8 +65,7 @@ struct hash_unweighted_tree
 			if (!has) return;
 			sort(all(z1));
 			sort(all(z2));
-			for (int i = 0; i < z1.size(); i++)
-			{
+			for (int i = 0; i < z1.size(); i++){
 				h1[u] = (h1[u] * (rnd[d[u]] + z1[i])) % mod1;
 				h2[u] = (h2[u] * (rnd[d[u]] + z2[i])) % mod2;
 			}
