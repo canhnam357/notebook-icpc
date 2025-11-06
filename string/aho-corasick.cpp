52b9848799c49bd59e380f8ac18fa272e8fcf489
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-11-06 20:41:21 +0700

    Update aho-corasick.cpp

--- string/aho-corasick.cpp
+++ string/aho-corasick.cpp
@@ -1,25 +1,25 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define all(x) x.begin(), x.end()
-// suffix_link : nhảy tới nút có độ dài nhỏ hơn mà lớn nhất và là suffix của chuỗi hiện tại (vì là trie nên chuỗi hiện tại là 1 prefix của một chuỗi đã thêm vào)
-// exit_link : là nút gần nhất sau khi nhảy (> 0) lần và nút đó là kết thúc của 1 trong những chuỗi đã thêm vào
+// suffix_link : nhay toi nut co do dai nho hon ma lon nhat va la suffix cua chuoi hien tai (vi la trie nen chuoi hien tai la 1 prefix cua mot chuoi da them vao)
+// exit_link : la nut gan nhat sau khi nhay (> 0) lan va nut do la ket thuc cua 1 trong nhung chuoi da them vao
+// trong ham get() tim nhung chuoi la suffix cua hien tai theo do dai giam dan
 struct aho_corasick{
     struct node{
-        int suffix_link = -1, exit_link = -1, nxt[26];
+        int suffix_link = -1, exit_link = -1, nxt[26], tot = 0, h = 0;
         bool has = false;
         node() {fill(nxt, nxt + 26, -1);}
     };
     vector<node> g = {node()};
-    void insert_string(const string &s){
+    void insert_string(const string &s, int point){
         int p = 0;
         for (char c: s){
-            if (g[p].nxt[c - 'a'] == -1){
-                g[p].nxt[c - 'a'] = g.size();
+            if (g[p].nxt[c - 'A'] == -1){
+                g[p].nxt[c - 'A'] = g.size();
                 g.emplace_back();
+                g.back().h = g[p].h + 1;
             }
-            p = g[p].nxt[c - 'a'];
+            p = g[p].nxt[c - 'A'];
         }
         g[p].has = true;
+        g[p].tot += point;
     }
     void build_automaton(){
         for (deque<int> q = {0}; q.size(); q.pop_front()){
@@ -35,39 +35,17 @@ struct aho_corasick{
             }
         }
     }
-    int feed(int p, int c) {
-        return g[p].nxt[c];
+    int get(int p) {
+        int res = 0;
+        int last = g[p].h;
+        for (int v = g[p].has ? p : g[p].exit_link; v != -1; v = g[v].exit_link) {
+            assert(g[v].h <= last);
+            last = g[v].h;
+            res += g[v].tot;
+        }
+        return res;
+    }
+    int feed(int u, int c) {
+        return g[u].nxt[c];
     }
 };
-
-signed main(){
-    cin.tie(0)->sync_with_stdio(0);
-    aho_corasick ac;
-    int n;
-    string z;
-    cin >> n >> z;
-    int m;
-    cin >> m;
-    vector<int> len;
-    for (int i = 0; i < m; i++)
-    {
-        string s;
-        cin >> s;
-        ac.insert_string(s, s.size());
-    }
-    ac.build_automaton();
-    ac.cc();
-    vector<int> dp(n, 123123123);
-    for (int r = 0, p = 0; r < n; r++)
-    {
-        p = ac.g[p].nxt[z[r] - 'a'];
-        for (int j: ac.get_sindex(p))
-        {
-            int l = r - j + 1;
-            if (l == 0) dp[r] = min(dp[r], 1);
-            else dp[r] = min(dp[r], dp[l - 1] + 1);
-        }
-    }
-    cout << dp[n - 1];
-    return 0;
-}
