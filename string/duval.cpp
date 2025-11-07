205f09ff2845da0b3363e7a29020afba13adc589
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-11-07 16:46:44 +0700

    Update duval.cpp

--- string/duval.cpp
+++ string/duval.cpp
@@ -1,9 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define all(x) x.begin(), x.end()
 template <typename T>
-int duval(int n, const T &s) 
-{
+int duval(int n, const T &s) {
   assert(n >= 1);
   int i = 0, ans = 0;
   while (i < n) {
@@ -24,20 +20,7 @@ int duval(int n, const T &s)
   return ans;
   // returns 0-indexed position of the least cyclic shift
 }
-
 template <typename T>
-int duval(const T &s) 
-{
+int duval(const T &s)  {
   return duval((int) s.size(), s);
-}
-int32_t main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	string s;
-	cin >> s;
-	int p = duval(s);
-	int n = s.size();
-	for (int i = 0; i < n; i++) cout << s[(i + p) % n];
-	return 0;
 }
