88e8d80953955d91b5995ed83229a28bd1826aaf
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-11-07 16:50:13 +0700

    Update extended-euclid.cpp

--- number-theory/extended-euclid.cpp
+++ number-theory/extended-euclid.cpp
@@ -1,24 +1,17 @@
-#include <bits/stdc++.h>
-#define int long long
-using namespace std;
-
 // Dung extended euclid de tim nghiem cua phuong trinh
 // a*x + b*y = gcd(a, b)
 // return gcd va 2 tham chieu x, y
 // Phuong trinh tong quat co nghiem khi d chia het cho gcd(a, b)
 int extended_euclid(int a, int b, int& x, int& y) {
     if (b == 0) {
-        x = 1;
-        y = 0;
+        x = 1; y = 0;
         return a;
     }
     int x1, y1;
     int d = extended_euclid(b, a % b, x1, y1);
-    x = y1;
-    y = x1 - y1 * (a / b);
+    x = y1; y = x1 - y1 * (a / b);
     return d;
 }
-
 // Tinh nghich dao su dung extended euclid
 int inverse_extended_euclid(int a, int mod) {
     int x, y;
