8a6aeecfd5ff98cbe2b8ffe42b66379b3e0c76ad
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2025-12-11 07:31:20 +0700

    Add main.cpp with basic program structure
    Initial implementation of main.cpp with basic setup.

---
+++ misc/main.cpp
@@ -0,0 +1,23 @@
+// pragma define
+// #pragma GCC optimize("O3")
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+#define MASK(i) (1LL << (i))
+#define MASK_ALL(i) ((1LL << (i+1)) - 1)
+#define sqr(x) ((x) * (x))
+const int inf = 1e18;
+const int mod = 998244353;
+const double eps = 1e-6;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef LOCAL
+       freopen("input.txt", "r", stdin);
+       freopen("output.txt", "w", stdout);
+    #endif
+}
