0711ffe636569c524a3c03809d5671a0cef620cd
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2026-02-07 13:29:40 +0700

    Refactor FFT implementation for clarity and efficiency

--- number-theory/fft.cpp
+++ number-theory/fft.cpp
@@ -1,37 +1,47 @@
-typedef complex<double> cd;
-const double pi = acos(-1);
-void fft(vector<cd>& a, bool invert) {
+using cd = complex<double>;
+const double PI = acos(-1);
+void fft(vector<cd> & a, bool invert) {
     int n = a.size();
-    if(n == 1)
-        return;
-    vector<cd> a0(n / 2), a1(n / 2);
-    for (int i = 0; i < n/2; i++) {
-        a0[i] = a[2*i]; a1[i] = a[2*i+1];
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        while(j & bit) {
+            j ^= bit;
+            bit >>= 1;
+        }
+        j ^= bit;
+        if (i < j) swap(a[i], a[j]);
     }
-    fft(a0, invert); fft(a1, invert);
-    // Chon w sao cho w ^ n = 1
-    double angle = 2 * pi / n;
-    if(invert)
-        angle = -angle;
-    cd w(cos(angle), sin(angle));
-    cd wi(1);
-    for(int i = 0; i < n/2; i++) {
-        a[i] = a0[i] + wi*a1[i];
-        a[i+n/2] = a0[i] - wi*a1[i];
-        wi *= w;
-        if(invert) {
-            a[i] /= 2; a[i+n/2] /= 2;
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2*PI / len;
+        if(invert) ang = -ang;
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i + j];
+                cd v = a[i + j + len/2] * w;
+                a[i + j] = u + v;
+                a[i + j + len/2] = u - v;
+                w *= wlen;
+            }
         }
     }
+    if (invert) {
+        for (cd & x : a) x /= n;
+    }
 }
+
 vector<double> multiply(const vector<double>& a, const vector<double>& b) {
     vector<cd> fa(a.begin(), a.end());
     vector<cd> fb(b.begin(), b.end());
     int n = 1;
     int sz = a.size() + b.size() - 1;
     while(n < a.size() + b.size()) n <<= 1;
-    fa.resize(n); fb.resize(n);
-    fft(fa, false); fft(fb, false);
+    fa.resize(n);
+    fb.resize(n);
+    fft(fa, false);
+    fft(fb, false);
     for(int i = 0; i < n; i++) fa[i] *= fb[i];
     fft(fa, true);
     vector<double> res(sz);
