ca89fe24ec228b224feb320c7e034467de629b42
Tran Canh Nam <canhnam357@gmail.com>
2026-02-28 09:34:39 +0700

    update aho-corasick

--- misc/modulo.cpp
+++ misc/modulo.cpp
@@ -1,13 +1,7 @@
-#include <bits/stdc++.h>
-#define int long long
-using namespace std;
-#define MASK(i) (1LL << (i))
-#define MASK_ALL(i) ((1LL << (i+1)) - 1)
-#define sqr(x) ((x) * (x))
-const int inf = 1e18;
 const int mod = 998244353;
-const double eps = 1e-6;
-
+const int N = 100000;
+int ft[N];
+int ift[N];
 int power(int a, int n) {
     int ans = 1;
     while(n) {
@@ -19,14 +13,9 @@ int power(int a, int n) {
     }    
     return ans;
 }
-
 int inverse(int a) {
     return power(a, mod-2);
 }
-
-const int N = 100000;
-int ft[N];
-int ift[N];
 void precalc() {
     ft[0] = 1;
     for(int i = 1; i < N; i++) {
@@ -36,21 +25,7 @@ void precalc() {
     for(int i = N-2; i >= 0; i--) {
         ift[i] = ift[i+1] * (i+1) % mod;
     }
-    // testing
-    // cerr << ift[1] << "\n";
 }
-
 int C(int n, int k) {
     return ft[n] * ift[k] % mod * ift[n-k] % mod;
-}
-
-int32_t main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    #ifdef LOCAL
-       freopen("input.txt", "r", stdin);
-       freopen("output.txt", "w", stdout);
-    #endif
-    precalc();
-    cerr << C(6, 3) << "\n";
 }
--- string/aho-corasick.cpp
+++ string/aho-corasick.cpp
@@ -1,30 +1,31 @@
 // suffix_link : nhay toi nut co do dai nho hon ma lon nhat va la suffix cua chuoi hien tai (vi la trie nen chuoi hien tai la 1 prefix cua mot chuoi da them vao)
 // exit_link : la nut gan nhat sau khi nhay (> 0) lan va nut do la ket thuc cua 1 trong nhung chuoi da them vao
 // trong ham get() tim nhung chuoi la suffix cua hien tai theo do dai giam dan
+const int M = 26;
 struct aho_corasick{
     struct node{
-        int suffix_link = -1, exit_link = -1, nxt[26], tot = 0;
+        int suffix_link = -1, exit_link = -1, nxt[M], tot = 0;
         bool has = false;
-        node() {fill(nxt, nxt + 26, -1);}
+        node() {fill(nxt, nxt + M, -1);}
     };
     vector<node> g = {node()};
-    void insert_string(const string &s, int point){
+    void insert_string(const string &s){
         int p = 0;
         for (char c: s){
-            if (g[p].nxt[c - 'A'] == -1){
-                g[p].nxt[c - 'A'] = g.size();
+            if (g[p].nxt[c - 'a'] == -1){
+                g[p].nxt[c - 'a'] = g.size();
                 g.emplace_back();
             }
-            p = g[p].nxt[c - 'A'];
+            p = g[p].nxt[c - 'a'];
         }
         g[p].has = true;
-        g[p].tot += point;
+        g[p].tot++;
     }
     void build_automaton(){
         for (deque<int> q = {0}; q.size(); q.pop_front()){
             int v = q.front(), suffix_link = g[v].suffix_link;
             if (v) g[v].exit_link = g[suffix_link].has ? suffix_link : g[suffix_link].exit_link;
-            for (int i=0; i< 26; i++){
+            for (int i=0; i < M; i++){
                 int &nxt = g[v].nxt[i], nxt_sf = v ? g[suffix_link].nxt[i] : 0;
                 if (nxt == -1) nxt = nxt_sf;
                 else{
